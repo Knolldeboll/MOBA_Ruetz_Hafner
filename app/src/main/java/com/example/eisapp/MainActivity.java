15a127aa082b614d69dc2dd24af0c3c708076332
Christian Ruetz <chrissi.ruetz@freenet.de>
2022-05-26 15:41:16 +0200

    RV test
    Recyclerview test
    - Momentan wird nur einer angezeigt!

--- app/src/main/java/com/example/eisapp/MainActivity.java
+++ app/src/main/java/com/example/eisapp/MainActivity.java
@@ -1,25 +1,55 @@
 package com.example.eisapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
 
 import com.example.eisapp.model.Economy;
 import com.example.eisapp.model.Eis;
+import com.example.eisapp.model.EisAdapter;
 import com.example.eisapp.model.PaymentHandler;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     //Daten von Model per Observer übergeben
     Economy eco;
     PaymentHandler pay;
+    List<Eis> eises;
+    EisAdapter ea;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        eises = new ArrayList<Eis>();
 
-        eco = Economy.getInstance();
-        pay = new PaymentHandler();
+        //Dummydaten erzeugen
+        //eises.add(new Eis("popel"));
+        eises.add(new Eis("schoko"));
+        eises.add(new Eis("erdbeer"));
+
+        // Rv holen
+        RecyclerView rv = (RecyclerView) findViewById(R.id.recv);
+
+        // Adapter für die daten erzeugen
+        ea = new EisAdapter(eises);
+
+        // beim rv den adapter setzen
+
+        rv.setAdapter(ea);
+
+        //rv.setHasFixedSize(true);
+
+        //Layoutmanager setzen
+        rv.setLayoutManager(new LinearLayoutManager(this));
+
+        //eco = Economy.getInstance();
+        //pay = new PaymentHandler();
 
         // addSoldIce wird dann von den ActionHandlern der Buttons aufgerufen
         //Dies alles ist nur zum testen
@@ -27,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
         // Eis adden blablabla
 
         //Dann : press auf zahlen!
-        pay.currentSum = eco.getCurrentValue();
+        //pay.currentSum = eco.getCurrentValue();
         // Wenn summenfeld geändert: ändere auch currentvalue im pay
         //pay.currentSum = inputfieldsum.value oder so
         //Tagestrinkgeld errechnet sich dann aus daylist.getsum - dailysum
@@ -35,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
         //gegeben entern!
         // on enter:
         //pay.getChange(inputfieldgegeben.value)
-        System.out.println(pay.getChange(50));
+        //System.out.println(pay.getChange(50));
 
 
         // Wenn <0: nochmal! zu wenig gegeben!
@@ -43,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
         // Wenn >=0 : anzeige im rückgeldfeld
         //Dann : pay
 
-        pay.pay();
+        //pay.pay();
 
 
 
---
+++ app/src/main/java/com/example/eisapp/model/EisAdapter.java
@@ -0,0 +1,81 @@
+package com.example.eisapp.model;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.eisapp.R;
+
+import java.util.List;
+
+
+public class EisAdapter extends RecyclerView.Adapter<EisAdapter.ViewHolder> {
+
+
+    // Der viewholder stellt struktur zur späteren speicherung der view, wie sie in der xml definiert ist
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView t1;
+        public TextView t2;
+        public TextView t3;
+
+        public ViewHolder(View itemView){
+
+            // Wie mit variablen anzahlen ?
+            // Evtl ein
+            super(itemView);
+
+            // Die textviews aus der xml holen
+            t1 = (TextView) itemView.findViewById(R.id.textView1);
+            t2 = (TextView) itemView.findViewById(R.id.textView2);
+            t3 = (TextView) itemView.findViewById(R.id.textView3);
+        }
+
+    }
+
+    // Hier werden die anzuzeigenden eise gespiechert
+    private List<Eis> eisliste;
+
+    // Beim erstellen des Objekts die eisliste hier befüllen mit werten von aussen, z.b. aus dem model
+    public EisAdapter(List<Eis> eisList){eisliste = eisList; }
+
+    // hier wird beim initialisieren das tatsächliche layout (view) in den viewholder gespeichert!
+    @Override
+    public EisAdapter.ViewHolder onCreateViewHolder( ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        // Inflate the custom layout - dh das xml-layout hier initial anwenden, noch aber leer
+        View eisView = inflater.inflate(R.layout.itemlayout, parent, false);
+
+        // Return a new holder instance - hat dann das xmllayout gespeichert und inflated
+        ViewHolder viewHolder = new ViewHolder(eisView);
+        return viewHolder;
+    }
+
+    // Nun daten in den viewholder - wie werden die daten in die view gesetzt ?
+    // wird per position gemacht! bzw für alle
+    @Override
+    public void onBindViewHolder( ViewHolder holder, int position) {
+
+        // die anzuzeigenden daten wählen
+        Eis eis = eisliste.get(position);
+        //hier dann die im viewholder gespeicherten elemente aus dem layout holen und bestücken
+        TextView t1 = holder.t1;
+        TextView t2 = holder.t2;
+        TextView t3 = holder.t3;
+        t1.setText(eis.name);
+        t2.setText(R.string.preis);
+        t3.setText(Float.toString(eis.preis));
+    }
+
+    @Override
+    public int getItemCount() {
+        return eisliste.size();
+    }
+}
