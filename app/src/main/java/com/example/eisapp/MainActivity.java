edf216c50343149c3f1c8ee7de25d83890db68a7
Christian Ruetz <chrissi.ruetz@freenet.de>
2022-07-09 09:58:53 +0200

    Improved menu fragment
    Added Buttons that lead to the fragment changes, disabled click-through

--- app/src/main/java/com/example/eisapp/MainActivity.java
+++ app/src/main/java/com/example/eisapp/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.eisapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import com.example.eisapp.model.AddEisFragment;
@@ -15,6 +20,7 @@ import com.example.eisapp.model.MarkenManager;
 import com.example.eisapp.model.Economy;
 import com.example.eisapp.model.Eis;
 import com.example.eisapp.model.MarkenAdapter;
+import com.example.eisapp.model.MenuFragment;
 import com.example.eisapp.model.PaymentHandler;
 import com.example.eisapp.model.SaleFragment;
 
@@ -27,10 +33,7 @@ public class MainActivity extends AppCompatActivity {
     PaymentHandler pay;
     List<Eis> eises;
     MarkenAdapter ma;
-    Button b1;
-    Button b2;
-    Button b3;
-    // TODO: Fragment in mainactivity +xml einbauen, recyclerview ins fragment
+       // TODO: Fragment in mainactivity +xml einbauen, recyclerview ins fragment
     // TODO: NavigationDrawer einbauen! (Evtl neues Projekt und kopieren ?)
 
 
@@ -59,108 +62,14 @@ public class MainActivity extends AppCompatActivity {
         pay = new PaymentHandler();
 
 
-       // Auslagern in Fragment ?!?
-    //    RecyclerView rv = (RecyclerView) findViewById(R.id.recv);
-    //    ma = new MarkenAdapter(markenManager.marken);
-
-        //Dummydaten erzeugen
-        //eises.add(new Eis("popel"));
-        /*eises.add(new Eis("schoko"));
-        eises.add(new Eis("erdbeer"));
-        eises.add(new Eis("derdbeer"));
-        eises.add(new Eis("ferdbeer"));
-        eises.add(new Eis("rerdbeer"));
-        eises.add(new Eis("kerdbeer"));
-        eises.add(new Eis("perdbeer"));
-        eises.add(new Eis("äerdbeer"));
-        */
-        // Rv holen
-
-
-        // Adapter für die daten erzeugen
-
-
-
-        // beim rv den adapter setzen
-
-
-        // IDEE FÜR DYNAMISCHE Unterlisten pro marken-"Item" : Recyclerview im recyclerview, oder einfach gridview darin
-
-    //    rv.setAdapter(ma);
-
-    //    rv.setHasFixedSize(true);
-
-        //Layoutmanager setzen
-    //    rv.setLayoutManager(new LinearLayoutManager(this));
-
-        // Für den child-rv: hä
-    //    layoutManager= new GridLayoutManager(this,3);
 
         SaleFragment saleFragment = new SaleFragment();
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.framemain, saleFragment, "salefrag");
         fragmentTransaction.commit();
 
-        // Testbutton!
-        b1 = (Button) findViewById(R.id.abschlussbutton);
-        b2 = (Button) findViewById(R.id.neueisbutton);
-        b3 = (Button) findViewById(R.id.homebutton);
-
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                // TODO: Die wird von Jojos menü erledigt!
-                Economy.getInstance().finishCurrentSale();
-                /*
-                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-                Fragment tempfrag = getSupportFragmentManager().findFragmentByTag("menufrag");
-                if(tempfrag != null){
-                    fragmentTransaction.remove(tempfrag);
-                }else{
-                    MenuFragment menuFragment = new MenuFragment();
-                    fragmentTransaction.add(R.id.framemenu,menuFragment,"menufrag");
-                }
 
 
-                fragmentTransaction.commit();
-                 */
-                FinishFragment ff = new FinishFragment();
-                FragmentTransaction fragmentTransaction1 = getSupportFragmentManager().beginTransaction();
-                fragmentTransaction1.replace(R.id.framemain,ff,"finishfrag");
-                fragmentTransaction1.commit();
-
-
-                /*   markenManager.removeBrand(markenManager.marken.get(markenManager.marken.size()-1));
-                ma.notifyItemRemoved(ma.getItemCount());
-            */
-            }
-        });
-        // Neueis
-        b2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                AddEisFragment aef = new AddEisFragment();
-                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-                fragmentTransaction.replace(R.id.framemain, aef, "addeisfrag");
-                fragmentTransaction.commit();
-            }
-        });
-
-        b3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                SaleFragment saleFragment = new SaleFragment();
-                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-                fragmentTransaction.replace(R.id.framemain, saleFragment, "salefrag");
-                fragmentTransaction.commit();
-            }
-        });
-
-        // addSoldIce wird dann von den ActionHandlern der Buttons aufgerufen
-        //Dies alles ist nur zum testen
-
-        // Eis adden blablabla
 
         //Dann : press auf zahlen!
 
@@ -182,8 +91,6 @@ public class MainActivity extends AppCompatActivity {
         markenManager.removeBrand(markenManager.marken.get(4));
         markenManager.removeBrand(markenManager.marken.get(5));
 */
-        //eco.addSoldIce(markenManager.marken.get(1).sorten.get(1));
-
 
 
         pay.currentSum = eco.getCurrentValue();
@@ -207,15 +114,41 @@ public class MainActivity extends AppCompatActivity {
 
         eco.printDay();
 
-        // TODO: Fragment setzen!
-        /*
-        SaleFragment fragment = new SaleFragment();
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.replace(R.id.framemain, fragment, "fragment1");
-        fragmentTransaction.commit();*/
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+      // Nur ein Menuitem, deshalb keine Fallunterscheidung
+
+
+
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                Fragment tempfrag = getSupportFragmentManager().findFragmentByTag("menufrag");
+                if(tempfrag != null){
+                    // Schließen
+                    fragmentTransaction.remove(tempfrag);
+
+                }else{
+                    // Öffnen
+                    MenuFragment menuFragment = new MenuFragment();
+                    fragmentTransaction.add(R.id.framemenu,menuFragment,"menufrag");
+
+
+                }
+
+
+                fragmentTransaction.commit();
+
+
+        return true;
+    }
 
 
 // Das vielleicht dann nicht ins fragment ?!?
--- app/src/main/java/com/example/eisapp/model/MenuFragment.java
+++ app/src/main/java/com/example/eisapp/model/MenuFragment.java
@@ -7,13 +7,17 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.eisapp.R;
 
 public class MenuFragment extends Fragment {
 
-    public static Button button1;
-
+    public static Button b1;
+    public static Button b2;
+    public static Button b3;
 
     public MenuFragment(){
 
@@ -25,14 +29,55 @@ public class MenuFragment extends Fragment {
 
     View view = inflater.inflate(R.layout.menufragmentlayout, container, false);
 
-    button1 = view.findViewById(R.id.menubutton1);
-    button1.setOnClickListener(new View.OnClickListener(){
+    b1 = (Button) view.findViewById(R.id.abschlussbutton);
+    b2 = (Button) view.findViewById(R.id.neueisbutton);
+    b3 = (Button) view.findViewById(R.id.homebutton);
 
+    b1.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-        System.out.println("Menu button pressed");
+
+            // TODO: Die wird von Jojos menü erledigt!
+
+            Economy.getInstance().finishCurrentSale();
+
+            FinishFragment ff = new FinishFragment();
+            FragmentTransaction fragmentTransaction1 = getParentFragmentManager().beginTransaction();
+            fragmentTransaction1.replace(R.id.framemain,ff,"finishfrag");
+            fragmentTransaction1.commit();
+
+
+                /*   markenManager.removeBrand(markenManager.marken.get(markenManager.marken.size()-1));
+                ma.notifyItemRemoved(ma.getItemCount());
+            */
         }
     });
+    // Neueis
+    b2.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            AddEisFragment aef = new AddEisFragment();
+            FragmentTransaction fragmentTransaction = getParentFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.framemain, aef, "addeisfrag");
+            fragmentTransaction.commit();
+        }
+    });
+
+    b3.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            SaleFragment saleFragment = new SaleFragment();
+            FragmentTransaction fragmentTransaction = getParentFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.framemain, saleFragment, "salefrag");
+            fragmentTransaction.commit();
+        }
+    });
+
+
+
+
+
+
 
     return view;
 }
